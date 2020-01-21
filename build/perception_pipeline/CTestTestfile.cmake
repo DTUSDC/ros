9b4a0607e8aea18d68e1c0e1fa5e9737e6f35f28
Ayaan Zaidi <biblioklept@icloud.com>
2020-01-21 18:41:47 +0530

    fixed gitignore

--- build/perception_pipeline/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/obviyus/Desktop/ROS/src/perception_pipeline
-# Build directory: /home/obviyus/Desktop/ROS/src/build/perception_pipeline
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
